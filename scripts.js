ebb88d4ebdad3dbfddb0fc5df049a63916f3201c
Kabirfandirma <kabirabbaalmustapha@gmail.com>
2025-01-05 10:20:25 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,5 @@
-// Smooth Scroll for Navigation Links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
-  
-  // Mobile Navigation Toggle
-  const navToggle = document.querySelector('.nav-toggle');
-  const navMenu = document.querySelector('nav ul');
-  
-  if (navToggle) {
-    navToggle.addEventListener('click', () => {
-      navMenu.classList.toggle('active');
-    });
-  }
-  
+// Toggle Dropdown Menu
+function toggleDropdown() {
+  const dropdownMenu = document.getElementById("dropdownMenu");
+  dropdownMenu.classList.toggle("show");
+}
